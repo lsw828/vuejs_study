8468599ec6059877de9675a14f83caf612864791
lsw828 <lsw828@gmail.com>
2019-12-14 16:47:25 +0900

    Add files via upload

---
+++ vue.js_official_guide/08_vuejs_bootcamp/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import BlueButton from './BlueButton'
+
+Vue.component('bluebutton', BlueButton);
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Using Single File Components'
+    }
+});
+
