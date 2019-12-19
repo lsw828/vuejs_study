f1564968ee6e5568b808927c5c97881535edf86a
lsw828 <lsw828@gmail.com>
2019-12-19 21:30:21 +0900

    hoho

---
+++ vue.js_official_guide/14_form_input_binding/index2.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    selected: '',
+    selected_multi: '',
+    selected_v_for: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' },
+    ]
+  },
+});
+
