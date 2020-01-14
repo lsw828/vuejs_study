f2032e32cad6768f768b359c72edb061dc7873c3
lsw828 <lsw828@gmail.com>
2020-01-15 01:40:53 +0900

    1st success...

--- vue-card-game/src/components/picCard.vue
+++ vue-card-game/src/components/picCard.vue
@@ -20,48 +20,127 @@
         </template>
       </v-img>
     </v-row>
-    {{ pic_id }}
+    {{ cardObj.pic_id }}
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+import { eventBus } from "@/main"
+
 export default {
   name: 'picCard',
   props: ['cardObj'],
+  computed: {
+    ...mapState(['cardObjArr', 'tryCount']),
+  },
   data() {
     return {
-      pic_id: null,
-      isCover: false,
       coverImgUrl: "https://pds.joins.com/news/component/htmlphoto_mmdata/201911/19/ce877ed2-0800-457f-b9a6-a86044718d40.jpg",
       srcUrl: null,
       lazySrcUrl: null,
     }
   },
   created() {
-    console.log("[picCard.vue] created()", this.cardObj.pic_id, this.cardObj.isCover)
+    this.flipCard()
 
-    this.pic_id = this.cardObj.pic_id
-    this.isCover = this.cardObj.isCover
-
-    this.srcUrl = "https://picsum.photos/200/120?image=" + this.pic_id
-    this.lazySrcUrl = "https://picsum.photos/20/12?image=" + this.pic_id
+    // [RECEIVER] temporary test code
+    eventBus.$on('allCardsBack', () => {
+      if(!this.cardObj.isDone) {
+        this.cardObj.isCover = true
+        this.flipCard()
+      }
+    })
   },
   methods: {
-    clicked() {
-      this.isCover = !this.isCover
-      
-      var cardObj = {}
-      cardObj["pic_id"] = this.pic_id
-      cardObj["isCover"] = this.isCover
-      this.$emit("selCardObj", cardObj)
+    ...mapActions(['actionClick']),
 
-      if(this.isCover) {
-        this.srcUrl = this.coverImgUrl
-        this.lazySrcUrl = this.coverImgUrl
-      } else {
-        this.srcUrl = "https://picsum.photos/200/120?image=" + this.pic_id
-        this.lazySrcUrl = "https://picsum.photos/20/12?image=" + this.pic_id
+    clicked() {
+      if(this.cardObj.isDone) {
+        return
       }
+      
+      this.cardObj.isCover = !this.cardObj.isCover
+
+      this.flipCard()
+
+      this.actionClick(this.cardObj)
+
+      var open_card_index = 0
+      var open_card = [{id:-1, pic_id:-1}, {id:-1, pic_id:-1}]
+      // front card 개수가 0, 1, 2
+      // isDone === false && isCover === true
+      var i = 0
+      for(i = 0; i < this.cardObjArr.length; i++) {
+        var card = this.cardObjArr[i]
+        if( (card.isDone === false) && (card.isCover === false) ) {
+          open_card[open_card_index]['id'] = card['id']
+          open_card[open_card_index]['pic_id'] = card['pic_id']
+          open_card_index++
+
+          if( open_card_index === 2 ) {
+            break
+          }
+        }
+      }
+
+      console.log(open_card_index)
+      console.log(open_card)
+
+      // 2 카드가 앞면이면... 
+      if( (open_card[0]['pic_id'] !== -1) && (open_card[1]['pic_id'] !== -1) ) {
+        // 2 카드가 맞으면
+        if( open_card[0]['pic_id'] === open_card[1]['pic_id'] ) {
+          for(i = 0; i < 2; i++) {
+            var id = open_card[i]['id']
+            //this.cardObjArr[id]['isDone'] = true
+            var j = 0
+            for(j = 0; j < this.cardObjArr.length; j++) {
+              var card = this.cardObjArr[j]
+              if( card.id === id ) {
+                card.isDone = true
+              }
+            }
+          }
+        } else {
+          const sleep = (milliseconds) => {
+           return new Promise(resolve => setTimeout(resolve, milliseconds))
+          }
+
+          sleep(1500).then(() => {
+            // [SENDER] temporary test code
+            console.log("eventBus.$emit('allCardsBack')")
+            eventBus.$emit("allCardsBack")
+          })
+        }
+      }
+
+      // all cards => isDone === true  => alert "Congratulation!"
+      for(i = 0; i < this.cardObjArr.length; i++) {
+        var card = this.cardObjArr[i]
+        if( card.isDone === false ) {
+          break
+        }
+      }
+
+      if( i === this.cardObjArr.length ) {
+        alert("Congratulation!", this.tryCount)
+      }
+    },
+    flipCard() {
+      if(this.cardObj.isCover) {
+        this.backCard()
+      } else {
+        this.frontCard()
+      }
+    },
+    frontCard() {
+      this.srcUrl = "https://picsum.photos/200/120?image=" + this.cardObj.pic_id
+      this.lazySrcUrl = "https://picsum.photos/20/12?image=" + this.cardObj.pic_id
+    },
+    backCard() {
+      this.srcUrl = this.coverImgUrl
+      this.lazySrcUrl = this.coverImgUrl
     }
   },
 }
--- vue-card-game/src/store/index.js
+++ vue-card-game/src/store/index.js
@@ -5,11 +5,76 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cardObjArr: [],
+    tryCount: 0,
+    prevCardObj: null
   },
+  // 간단한 설정 하나 바꾸는데, 꼭 actions -> mutations 순서로 거쳐가야 하나?
   mutations: {
+    addCard(state, cardObj) {
+      state.cardObjArr.push(cardObj)
+    },
+    shuffleCards(state) {
+      state.cardObjArr.sort(function(){return 0.5 - Math.random()})
+    },
   },
   actions: {
+    initCards(context) {
+      // 예외 처리...
+      if (context.state.cardObjArr.length >= 12) {
+        console.log("-> context.state.cardObjArr.length >= 12 -> return")
+        return 
+      }
+      
+      var pic_ids = [
+        441, 478, 249, 265, 299, 646,
+        441, 478, 249, 265, 299, 646,
+      ]
+
+      var i = 0
+      for (i = 0; i < pic_ids.length; i++) {
+        var cardObj = {}
+        cardObj["id"] = i
+        cardObj["pic_id"] = pic_ids[i]
+        cardObj["isCover"] = true
+        cardObj["isDone"] = false
+
+        context.commit('addCard', cardObj)
+      }
+    },
+    shuffleCards(context) {
+      console.log("[store/index.js] actions: shuffleCards()")
+      if (context.state.cardObjArr.length > 0) {
+        context.commit('shuffleCards')
+      }
+    },
+    actionClick(context, cardObj) {
+      // 점수 계산
+      console.log("[store/index.js] actions: actionClick()", cardObj.pic_id, cardObj.isCover)
+      
+      // if(context.state.prevCardObj === null) {
+      //   context.state.prevCardObj = { ...cardObj }
+      // } else {
+      //   if(context.state.prevCardObj.id === cardObj.id) {
+      //     context.state.prevCardObj = null
+      //   } else {
+      //     if(context.state.prevCardObj.pic_id === cardObj.pic_id) {
+      //       // make two card done
+      //       context.state.prevCardObj.isDone = true
+      //       cardObj.isDone = true
+      //     } else {
+      //       // set prev card -> null
+      //       context.state.prevCardObj = null
+            
+      //       // two cards -> back
+      //       context.state.prevCardObj.isCover = true
+      //       cardObj.isCover = true
+
+      //     }
+      //   }
+      // }
+
+      context.state.tryCount++
+    },
   },
-  modules: {
-  }
 })
